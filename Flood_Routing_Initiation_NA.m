0ba518150f4ee2ac4167f7459d7ff48ada58523b
behzadasd <58999767+behzadasd@users.noreply.github.com>
2019-12-17 21:16:00 -0500

    Update Flood_Routing_Initiation_NA.m

--- Flood_Routing_Initiation_NA.m
+++ Flood_Routing_Initiation_NA.m
@@ -1,11 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%  Flood Routing Grid-cells Initiation North America  %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Behzad Asadieh   , Ph.D. Candidate                  %%%
-%%% Civil Engineering Department - Water Resources      %%%
-%%% The City College of The City University of New York %%%
-%%% basadie00@citymail.cuny.edu                         %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Behzad Asadieh, Ph.D.           %%%
+%%% University of Pennsylvania      %%%
+%%% basadieh@sas.upenn.edu          %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 tic;
 clear;
 clc;
